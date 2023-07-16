0d258b1836cbcb1b331dba5ffed2d9c1a9e06b70
seonglae <sungle3737@gmail.com>
2023-07-16 17:59:03 +0900

    meta: first commit for angryface docs

---
+++ tokens.config.ts
@@ -0,0 +1,14 @@
+import { defineTheme } from 'pinceau'
+
+export default defineTheme({
+  docus: {
+    header: {
+      logo: {
+        height: {
+          initial: '0',
+          sm: '0'
+        }
+      }
+    }
+  }
+})
